4e5d2f4a13105841fa1be11991529c74254f6bfa
Mohit <sharmamohit2882003@gmail.com>
2025-09-01 17:31:53 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-blue-600">
+        Hello Tailwind CSS v3 in Next.js!
+      </h1>
+      <Button>Click me</Button>
+    </>
+  );
+}
