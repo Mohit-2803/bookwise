17ae22a5728eae8e574a9057a1b5b4e9508f4ff3
Mohit <sharmamohit2882003@gmail.com>
2025-09-01 19:01:41 +0530

    second

---
+++ components/BookOverview.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const BookOverview = ({
+  title,
+  author,
+  genre,
+  rating,
+  total_copies,
+  available_copies,
+  description,
+  cover,
+  color,
+}: Book) => {
+  return (
+    <section className="book-overview">
+      <div className="flex flex-1 flex-col gap-5">
+        <h1>{title}</h1>
+
+        <div className="book-info">
+          <p>
+            By <span className="font-semibold text-light-200">{author}</span>
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default BookOverview;
